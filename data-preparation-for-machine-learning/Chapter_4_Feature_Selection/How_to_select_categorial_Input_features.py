35deb50efe6a0c7f51ccdfb7f796c06ad78c4048
Jannis997 <113466457+Jannis997@users.noreply.github.com>
2022-09-23 12:44:22 +0200

    Update How_to_select_categorial_Input_features.py

--- data-preparation-for-machine-learning/Chapter_4_Feature_Selection/How_to_select_categorial_Input_features.py
+++ data-preparation-for-machine-learning/Chapter_4_Feature_Selection/How_to_select_categorial_Input_features.py
@@ -1,1 +1,16 @@
 
+# feature selection, finding the k best features using Chi2
+def select_features(X_train, y_train, X_test):
+fs = SelectKBest(score_func=chi2, k='all')
+fs.fit(X_train, y_train)
+X_train_fs = fs.transform(X_train)
+X_test_fs = fs.transform(X_test)
+return X_train_fs, X_test_fs, fs
+
+# feature selection with mutual info classicif
+def select_features(X_train, y_train, X_test):
+fs = SelectKBest(score_func=mutual_info_classif, k='all')
+fs.fit(X_train, y_train)
+X_train_fs = fs.transform(X_train)
+X_test_fs = fs.transform(X_test)
+return X_train_fs, X_test_fs, fs
